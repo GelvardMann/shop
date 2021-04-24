602d9c86df4f95db5ff1b894dfc0133a794bb299
GelvardMann <timoffey119@gmail.com>
2021-04-24 21:11:45 +0300

    Initial app

---
+++ common/config/main.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'bootstrap' => [
+        'common\modules\main\Bootstrap',
+        'common\modules\user\Bootstrap',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'common\modules\user\Module',
+        ],
+        'main' => [
+            'class' => 'common\modules\main\Module',
+        ],
+    ],
+];
---
+++ common/modules/main/controllers/backend/DefaultController.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace common\modules\main\controllers\backend;
+
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {}
+     */
+    public function actions(): array
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex(): string
+    {
+
+        return $this->render('index');
+    }
+}
